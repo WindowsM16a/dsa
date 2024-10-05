5609fc6b1d5bf719608fc1c9ea841e8a4d058726
WindowsM16a <wuvershayne@gmail.com>
2024-10-05 22:06:34 +0000

    complexity and big O

--- first.py
+++ first.py
@@ -24,24 +24,24 @@
 #cards contains repeating numbers
 #query occurs at more than one position in cards
 
-from jovian.pythondsa import evaluate_test_case
-
 
 def locate_card(cards, query):
     #create a variable, position, with value 0 
     position = 0
 
+    print('cards', cards)
+    print('query', query)
+
     #create a loop for repetition
-    while True:
+    while position < len(cards):
+        print('position:', position)
         if cards[position] == query:
             #answer found, return and exit
             return position
         #increment to position
         position += 1
         #check if we've iterated through the whole array
-        if cards[position] == len(cards):
-            #number wasn't found, return -1
-            return -1 
+    return -1 
 
 
 test = {
@@ -55,83 +55,92 @@ test = {
 tests = []
 #normal case
 tests.append(test)
-# #query is somewhere in the list
-# tests.append({
-#     'input':{
-#         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
-#         'query' : 1
-#     },
-#     'output' : 6
-# })
-# #query is first element
-# tests.append({
-#     'input':{
-#         'cards': [4, 2, 1, -1],
-#         'query' : 4
-#     },
-#     'output' : 0
-# })
-# #query is last element
-# tests.append({
-#     'input':{
-#         'cards': [3, -1, -9, -127],
-#         'query' : -127
-#     },
-#     'output' : 3
-# })
-# #query is last element
-# tests.append({
-#     'input':{
-#         'cards': [3, -1, -9, -127],
-#         'query' : -127
-#     },
-#     'output' : 3
-# })
-# #cards contains only query
-# tests.append({
-#     'input':{
-#         'cards': [6],
-#         'query' : 6
-#     },
-#     'output' : 0
-# })
-# #cards does not contain query
-# tests.append({
-#     'input':{
-#         'cards': [9, 7, 5, 2, -9],
-#         'query' : 4
-#     },
-#     'output' : -1
-# })
-# #cards is empty
-# tests.append({
-#     'input':{
-#         'cards': [],
-#         'query' : 7
-#     },
-#     'output' : -1
-# })
-# #repeating numbers in cards
-# tests.append({
-#     'input':{
-#         'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
-#         'query' : 3
-#     },
-#     'output' : 7
-# })
-# #query occurs multiple times..... will return the first instance of query appearing
-# tests.append({
-#     'input':{
-#         'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
-#         'query' : 6
-#     },
-#     'output' : 2
-# })
+#query is somewhere in the list
+tests.append({
+    'input':{
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query' : 1
+    },
+    'output' : 6
+})
+#query is first element
+tests.append({
+    'input':{
+        'cards': [4, 2, 1, -1],
+        'query' : 4
+    },
+    'output' : 0
+})
+#query is last element
+tests.append({
+    'input':{
+        'cards': [3, -1, -9, -127],
+        'query' : -127
+    },
+    'output' : 3
+})
+#query is last element
+tests.append({
+    'input':{
+        'cards': [3, -1, -9, -127],
+        'query' : -127
+    },
+    'output' : 3
+})
+#cards contains only query
+tests.append({
+    'input':{
+        'cards': [6],
+        'query' : 6
+    },
+    'output' : 0
+})
+#cards does not contain query
+tests.append({
+    'input':{
+        'cards': [9, 7, 5, 2, -9],
+        'query' : 4
+    },
+    'output' : -1
+})
+#cards is empty
+tests.append({
+    'input':{
+        'cards': [],
+        'query' : 7
+    },
+    'output' : -1
+})
+#repeating numbers in cards
+tests.append({
+    'input':{
+        'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query' : 3
+    },
+    'output' : 7
+})
+#query occurs multiple times..... will return the first instance of query appearing
+tests.append({
+    'input':{
+        'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query' : 6
+    },
+    'output' : 2
+})
 
-evaluate_test_case(locate_card, test)
+def run_tests(tests, locate_card):
+    for i, test in enumerate(tests):
+        print(f"Running test {i + 1}")
+        result = locate_card(test['input']['cards'], test['input']['query'])
+        if result == test['output']:
+            print("PASSED")
+        else:
+            print(f"the test failed. expected {test['output']}, but got {result}")
+        print()
 
-# result = locate_card(**test['input'])
-# print(result)
+run_tests(tests, locate_card)
 
-# isSolved = result == test['output']
-# print(isSolved)
+#THEY BOTH WORK, THE ONE ON TOP JUST WORKS BETTER
+
+# for test in tests:
+#     print(locate_card(**test['input']) == test['output'])
